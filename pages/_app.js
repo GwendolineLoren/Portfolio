0ea62364b6f6ff21083214b8c4d93f3b678aa9c3
GwendolineLoren <96466505+GwendolineLoren@users.noreply.github.com>
2025-11-14 00:01:50 +0200

    Refactor Google Analytics setup in _app.js
    Updated Google Analytics integration to GA4 and improved script loading.

--- pages/_app.js
+++ pages/_app.js
@@ -1,16 +1,18 @@
 // pages/_app.js
-
 import { useEffect } from "react";
 import { useRouter } from "next/router";
+import Script from "next/script";
 
 function MyApp({ Component, pageProps }) {
   const router = useRouter();
 
   useEffect(() => {
     const handleRouteChange = (url) => {
-      window.gtag("config", process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS, {
-        page_path: url,
-      });
+      if (window.gtag) {
+        window.gtag("config", "G-4QGYSTZEGK", {
+          page_path: url,
+        });
+      }
     };
     router.events.on("routeChangeComplete", handleRouteChange);
     return () => {
@@ -19,16 +21,35 @@ function MyApp({ Component, pageProps }) {
   }, [router.events]);
 
   return (
-    <div className="body">
-      <Component {...pageProps} />
-      <style jsx>{`
-        .body {
-          background-color: rgb(211, 209, 249)
-        }
-    `}</style>
-    </div>
-    
+    <>
+      {/* Google Analytics GA4 */}
+      <Script
+        strategy="afterInteractive"
+        src="https://www.googletagmanager.com/gtag/js?id=G-4QGYSTZEGK"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-4QGYSTZEGK', {
+            page_path: window.location.pathname,
+          });
+        `}
+      </Script>
+
+      {/* Your App */}
+      <div className="body">
+        <Component {...pageProps} />
+        <style jsx>{`
+          .body {
+            background-color: rgb(211, 209, 249);
+          }
+        `}</style>
+      </div>
+    </>
   );
 }
 
 export default MyApp;
+

e77bfb0129cf828bb96c145b3c7c7b6c5554aa54
GwendolineLoren <96466505+GwendolineLoren@users.noreply.github.com>
2025-11-13 23:25:02 +0200

    Remove Google Tag Manager from About page
    Removed Google Tag Manager scripts from the About page.

--- pages/about.js
+++ pages/about.js
@@ -17,16 +17,6 @@ const imgStyle = {
 export default () => (
   <Layout>
 
-  <!-- Google tag (gtag.js) -->
-  <script async src="https://www.googletagmanager.com/gtag/js?id=G-4QGYSTZEGK"></script>
-  <script>
-    window.dataLayer = window.dataLayer || [];
-    function gtag(){dataLayer.push(arguments);}
-    gtag('js', new Date());
-
-    gtag('config', 'G-4QGYSTZEGK');
-  </script>
-
     <div className="breadcrumbs">
       <a href="/">Home</a> {">"} <b>About</b>
     </div>
